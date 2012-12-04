54f63d2e490012500b941a04edc8c054aca098d5
mxro <mxro@nowhere.com>
2012-12-04 15:33:04 +1300

    adding licnese

--- src/main/java/com/appjangle/fragements/RandomString.java
+++ src/main/java/com/appjangle/fragements/RandomString.java
@@ -16,10 +16,12 @@ public class RandomString {
 	private static final char[] symbols = new char[36];
 
 	static {
-		for (int idx = 0; idx < 10; ++idx)
+		for (int idx = 0; idx < 10; ++idx) {
 			symbols[idx] = (char) ('0' + idx);
-		for (int idx = 10; idx < 36; ++idx)
+		}
+		for (int idx = 10; idx < 36; ++idx) {
 			symbols[idx] = (char) ('a' + idx - 10);
+		}
 	}
 
 	private final Random random = new Random();
@@ -39,4 +41,4 @@ public class RandomString {
 	}
 
 }
-//<!-- one.end -->
+// <!-- one.end -->
