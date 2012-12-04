0d1e43d27f8a6fe0928ce1e585cc0488800ff0a2
mxro <mxro@nowhere.com>
2012-12-04 15:28:05 +1300

    adding utility to determine random String

---
+++ src/main/java/com/appjangle/fragements/RandomString.java
@@ -0,0 +1,40 @@
+package com.appjangle.fragements;
+
+import java.util.Random;
+
+/**
+ * from http://stackoverflow.com/questions/41107/how-to-generate-a-random-alpha-
+ * numeric-string-in-java
+ * 
+ * author: http://stackoverflow.com/users/3474/erickson
+ * 
+ * 
+ */
+public class RandomString {
+
+	private static final char[] symbols = new char[36];
+
+	static {
+		for (int idx = 0; idx < 10; ++idx)
+			symbols[idx] = (char) ('0' + idx);
+		for (int idx = 10; idx < 36; ++idx)
+			symbols[idx] = (char) ('a' + idx - 10);
+	}
+
+	private final Random random = new Random();
+
+	private final char[] buf;
+
+	public RandomString(final int length) {
+		if (length < 1)
+			throw new IllegalArgumentException("length < 1: " + length);
+		buf = new char[length];
+	}
+
+	public String nextString() {
+		for (int idx = 0; idx < buf.length; ++idx)
+			buf[idx] = symbols[random.nextInt(symbols.length)];
+		return new String(buf);
+	}
+
+}
