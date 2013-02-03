2adf77d692973aa404f4474baf32f2cd0144575c
mxro <mxro@nowhere.com>
2013-02-04 09:48:41 +1300

    adding fragment stream to string

--- src/main/java/com/appjangle/fragements/Streams.java
+++ src/main/java/com/appjangle/fragements/Streams.java
@@ -1,11 +1,25 @@
 package com.appjangle.fragements;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class Streams {
 
+	// <!-- one.upload https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/fragements-stream-to-string -->
+	public static String toString(final InputStream inputStream)
+            throws IOException {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        final byte[] buffer = new byte[1024];
+        int length = 0;
+        while ((length = inputStream.read(buffer)) != -1) {
+            baos.write(buffer, 0, length);
+        }
+        return new String(baos.toByteArray());
+    }
+	// <!-- one.end -->
+	
 	// <!-- one.upload https://u1.linnk.it/qc8sbw/usr/apps/textsync/files/fragements-copy-stream -->
 	/**
 	 * Copy Streams from Apache Commons
